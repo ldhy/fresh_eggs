86541dc5e80072e53367e9e0752b6578e1dda8cd
ldhy <laura.delahaye@gmail.com>
2018-03-01 16:47:43 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Fresh Eggs</title>
+    </head>
+
+    <body>
+      <h1>Fresh Eggs</h1>
+      <h2>Mes oeufs sont-ils frais, archi frais ?</h2>
+
+      <h3>Je veux connaître :</h3>
+
+      <h4>La date max en extra frais de mes oeufs :</h4>
+        <form>
+          <label>Je rentre la DCR de mes oeufs :</label>
+          <input type="date" />
+        </form>
+
+      <h4>La DCR max à choisir pour des oeufs extra frais :</h4>
+          <form>
+            <label>Je rentre la date limite pour laquelle je veux des oeufs extra frais :</label>
+            <input type="date" />
+          </form>
+
+    </body>
+</html>
